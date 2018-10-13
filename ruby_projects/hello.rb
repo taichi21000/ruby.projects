92be8ff4f5ff1e5180a83f234834f4570e1c23d5
taichi21000 <42987100+taichi21000@users.noreply.github.com>
2018-10-13 17:19:16 +0900

    Add files via upload

---
+++ ruby_projects/hello.rb
@@ -0,0 +1,41 @@
+#ruby 章末問題
+#問題１
+puts "first ruby"
+#問題２
+puts "Ruby" + "Started"
+#問題３
+puts 3 + 3
+puts 3 - 3
+puts 3 * 3
+puts 3 / 3
+#問題４,5,6
+
+array = [1,2,3,4]
+
+p array[0]
+p array[1]
+p array[2]
+p array[3]
+
+#問題７
+
+person = { name: '中谷太一', birthday: '5月15日', blood_type: 'B+'}
+
+puts person[:name]
+puts person[:birthday]
+puts person[:blood_type]
+
+#問題８
+def print_method(what, program)
+  print(what + "," + program )
+end
+ print_method("like", "Ruby")
+
+def print(word)
+  p plus_ruby("yhee")
+end
+
+
+#問題９
+p 4.next
+p 10.next
